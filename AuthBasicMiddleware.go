1e1ee8718dc008a499bbd3105c0a5e801d76c7a6
Daniel <daniel.unverricht@pentlandfirth.com>
2019-02-05 15:02:43 +0100

    add authbasic

---
+++ AuthBasicMiddleware.go
@@ -0,0 +1,32 @@
+package uauth
+
+import (
+	"context"
+	"crypto/subtle"
+	"encoding/json"
+	"net/http"
+)
+
+// AuthBasic <-
+func AuthBasic(username string, password string, realm string) func(next http.HandlerFunc) http.HandlerFunc {
+	return func(next http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+
+			user, pass, ok := r.BasicAuth()
+
+			if !ok || subtle.ConstantTimeCompare([]byte(user), []byte(username)) != 1 || subtle.ConstantTimeCompare([]byte(pass), []byte(password)) != 1 {
+				js, _ := json.Marshal(map[string]string{
+					"error": "unauthorized",
+				})
+				w.Header().Add("Content-Type", "application/json")
+				w.Header().Add("WWW-Authenticate", `Basic realm="`+realm+`"`)
+				w.WriteHeader(401)
+				w.Write(js)
+				return
+			}
+
+			ctx := context.WithValue(r.Context(), CtxKeyUser, user)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		}
+	}
+}
