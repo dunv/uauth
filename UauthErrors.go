af512a02bbe341996ebc9608069b82721a166e66
Daniel <daniel.unverricht@pentlandfirth.com>
2021-07-25 15:38:38 +0200

    Add error for permissions

--- UauthErrors.go
+++ UauthErrors.go
@@ -4,6 +4,7 @@ import "errors"
 
 var ErrInvalidRefreshToken error = errors.New("ErrInvalidRefreshToken")
 var ErrInvalidUser error = errors.New("ErrInvalidUser")
+var ErrInsufficientPermissions error = errors.New("ErrInsufficientPermissions")
 
 func MachineError(readable error, details error) map[string]interface{} {
 	return map[string]interface{}{
